c6ce0bc3a4c3202be5b2c766847229a7cbd58031
cwilson2121 <50244082+cwilson2121@users.noreply.github.com>
2019-05-03 12:45:23 -0400

    Add files via upload

--- ColorMap.java
+++ ColorMap.java
@@ -1,6 +1,24 @@
 import java.util.*;
 public class ColorMap
 {
+    private static int ColorCount;
+    
+    public boolean Color
+    {
+    
+    }
     public static void main(String[] args)
     {
+        
+        //Read in adjacnecy list
+        
+        for(int i = 0; i < vertexCount; i++) //Find vertexCount somehow
+        {
+            
+        }
+            
          
+         
+        Color(graph);
+        System.out.println("This graph has a minimum of " + ColorCount + " colors.")
+    }
